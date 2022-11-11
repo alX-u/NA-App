abf3d2da79a42ef912b9240064233a20f991c88c
Alejandro V <70179001+alX-u@users.noreply.github.com>
2022-11-10 21:36:53 -0500

    Avances

--- peces_app/lib/pages/muestreos/AddMuestreoSiembra.dart
+++ peces_app/lib/pages/muestreos/AddMuestreoSiembra.dart
@@ -2,7 +2,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:peces_app/domain/controllers/user_controller.dart';
+import 'package:peces_app/service/sheets.dart';
 
+import '../../model/UserFields.dart';
+import '../general/GeneralPage.dart';
 import '../general/ResumenMuestreoPage.dart';
 
 class AddMuestreoSiembra extends StatefulWidget {
@@ -18,6 +21,8 @@ class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
       TextEditingController();
   final TextEditingController _pesoSiembraPorUnidadController =
       TextEditingController();
+  DateTime? _fechaController;
+  String? fecha;
 
   @override
   Widget build(BuildContext context) {
@@ -65,18 +70,18 @@ class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
                   children: [
                     Text('Registra tu muestreo de siembra: ',
                         style: estiloTexto(28)),
-                    const SizedBox(height: 10),
                     const SizedBox(height: 30),
                     //Input de los peces sembrados
-                    inputLabel('Peces Sembrados:'),
-                    const SizedBox(height: 15),
-                    formField(_pecesSembradosController),
+                    formField('Peces Sembrados: ', _pecesSembradosController),
                     const SizedBox(height: 20),
                     //Input del peso de la siembra por unidad
-                    inputLabel('Peso de la siembra por unidad (en Gr):'),
-                    const SizedBox(height: 15),
-                    formField(_pesoSiembraPorUnidadController),
+                    formField('Peso de la siembra por unidad (en Gr):',
+                        _pesoSiembraPorUnidadController),
                     const SizedBox(height: 20),
+                    //Fecha
+                    botonFecha(),
+                    const SizedBox(height: 20),
+                    botonEnviar()
                   ],
                 ),
               )
@@ -101,18 +106,34 @@ class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
         ]);
   }
 
-//Widget de los form fields donde el usuario colocará su información
-  Widget formField(TextEditingController controller) {
+  //Widget de los form fields donde el usuario colocará su información
+  Widget formField(String textname, TextEditingController controller) {
     return Container(
       height: 55,
       width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(13)),
+      /*decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(13)),*/
       child: TextFormField(
         controller: controller,
-        decoration: const InputDecoration(
-            border: InputBorder.none,
-            contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 15)),
+        decoration: InputDecoration(
+            labelText: textname,
+            labelStyle: const TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+                shadows: <Shadow>[
+                  Shadow(
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 3.0,
+                      color: Color.fromARGB(255, 0, 0, 0))
+                ]),
+
+            // border: InputBorder.none,
+            focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 1)),
+            enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 1.3))
+            //contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 15)
+            ),
         style: const TextStyle(
           color: Colors.black,
           fontSize: 16,
@@ -134,5 +155,130 @@ class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
                   color: Color.fromARGB(255, 0, 0, 0))
             ],
             letterSpacing: 0.2));
+  }
+
+  //Función para la fecha
+  Future pickDate() async {
+    //Escogemos fecha inicial como el día de hoy
+    final initialDate = DateTime.now();
+    //Escogemos una nueva fecha
+    final newDate = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: DateTime(DateTime.now().year - 10),
+        lastDate: DateTime(DateTime.now().year + 10));
+    //Revisamos si es nulo
+    if (newDate == null) return;
+    //Seteamos el controller de la fecha como la newDate
+    setState(() {
+      _fechaController = newDate;
+      //Enviaremos la fecha como un string al spreadsheet
+      fecha =
+          '${_fechaController!.year.toString()}/${_fechaController!.month.toString()}/${_fechaController!.day.toString()}';
+      debugPrint('Soy la fecha que vas a enviar: ' + fecha!);
+    });
+  }
+
+  //Función para el texto que se verá en el botón de escoger fecha
+  String getTextFecha() {
+    if (_fechaController == null) {
+      return 'Seleccione una fecha';
+    } else {
+      return '${_fechaController!.year}/${_fechaController!.month}/${_fechaController!.day}';
+    }
+  }
+
+  //Botón para escoger fecha
+  Widget botonFecha() {
+    return InkWell(
+      onTap: () {
+        pickDate();
+      },
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(13),
+            color: Color.fromARGB(255, 40, 102, 173)),
+        child: Center(
+          child: Text(
+            getTextFecha(),
+            style: estiloTexto(18),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //Widget del botón para enviar el muestreo
+  Widget botonEnviar() {
+    return InkWell(
+      onTap: () async {
+        String nombreLote = userController.userLote;
+        //Inicializamos la clase sheetsAPI, pasando el email del usuario, esto decide la spreadsheet y la worksheet
+        await sheetsAPI.init(userController.userEmail, nombreLote);
+        //Establecemos la información que vamos a pasar al spreadsheet en formato JSON
+        final info = {
+          UserFields.pecesSembrados: _pecesSembradosController.text.trim(),
+          UserFields.pesoSiembraPorUnidad:
+              _pesoSiembraPorUnidadController.text.trim(),
+          UserFields.fecha: fecha,
+        };
+        //Nos aseguramos de que la información no esté vacía
+        if (_fechaController != null) {
+          try {
+            //Llamamos a la función que añadirá la información a la spreadsheet
+            await sheetsAPI.insert([info]);
+            //Se mostrará después de realizar el registro
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: const Text("Muestreo registrado exitósamente"),
+                      content: const Text("Gracias por registrar su muestreo"),
+                      actions: <Widget>[
+                        TextButton(
+                            onPressed: () {
+                              //Vamos hacia la página de Inicio de Sesión
+                              Navigator.pop(context, true);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const GeneralPage()));
+                              //Borramos el texto escrito en los textfield
+                              _fechaController == null;
+                            },
+                            child: const Text("OK"))
+                      ],
+                    ));
+          } on Exception catch (e) {
+            final snackbar = SnackBar(content: Text(e.toString()));
+            ScaffoldMessenger.of(context).showSnackBar(snackbar);
+          }
+        } else {
+          const snackbar = SnackBar(
+              content: Text('Asegúrese de que todos los campos estén llenos'));
+          ScaffoldMessenger.of(context).showSnackBar(snackbar);
+        }
+      },
+      child: Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            gradient: const LinearGradient(colors: [
+              Color(0xFFEC3E1E),
+              Color(0xFFDE300B),
+              Color(0xFFC40806)
+            ]),
+            borderRadius: BorderRadius.circular(13),
+            color: Colors.white),
+        child: Center(
+          child: Text(
+            'Enviar Muestreo',
+            style: estiloTexto(18),
+          ),
+        ),
+      ),
+    );
   }
 }
