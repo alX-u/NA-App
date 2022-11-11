01f4bfcc15c568d2ea5f02fae11ac4583c6b5eef
Alejandro V <70179001+alX-u@users.noreply.github.com>
2022-11-10 21:01:09 -0500

    Update AddMuestreoSiembra.dart

--- peces_app/lib/pages/muestreos/AddMuestreoSiembra.dart
+++ peces_app/lib/pages/muestreos/AddMuestreoSiembra.dart
@@ -14,6 +14,10 @@ class AddMuestreoSiembra extends StatefulWidget {
 
 class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
   UserController userController = Get.find();
+  final TextEditingController _pecesSembradosController =
+      TextEditingController();
+  final TextEditingController _pesoSiembraPorUnidadController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +63,20 @@ class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    
+                    Text('Registra tu muestreo de siembra: ',
+                        style: estiloTexto(28)),
+                    const SizedBox(height: 10),
+                    const SizedBox(height: 30),
+                    //Input de los peces sembrados
+                    inputLabel('Peces Sembrados:'),
+                    const SizedBox(height: 15),
+                    formField(_pecesSembradosController),
+                    const SizedBox(height: 20),
+                    //Input del peso de la siembra por unidad
+                    inputLabel('Peso de la siembra por unidad (en Gr):'),
+                    const SizedBox(height: 15),
+                    formField(_pesoSiembraPorUnidadController),
+                    const SizedBox(height: 20),
                   ],
                 ),
               )
@@ -69,15 +86,53 @@ class _AddMuestreoSiembraState extends State<AddMuestreoSiembra> {
       ),
     );
   }
-}
 
-//---------------------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------------------------
 // Widgets, métodos y herramientas utilizadas
-TextStyle estiloTexto(double size) {
-  return TextStyle(color: Colors.white, fontSize: size, shadows: const <Shadow>[
-    Shadow(
-        offset: Offset(2.0, 2.0),
-        blurRadius: 3.0,
-        color: Color.fromARGB(255, 0, 0, 0))
-  ]);
+  TextStyle estiloTexto(double size) {
+    return TextStyle(
+        color: Colors.white,
+        fontSize: size,
+        shadows: const <Shadow>[
+          Shadow(
+              offset: Offset(2.0, 2.0),
+              blurRadius: 3.0,
+              color: Color.fromARGB(255, 0, 0, 0))
+        ]);
+  }
+
+//Widget de los form fields donde el usuario colocará su información
+  Widget formField(TextEditingController controller) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(13)),
+      child: TextFormField(
+        controller: controller,
+        decoration: const InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 15)),
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+//Widgets y Métodos
+  Widget inputLabel(String input) {
+    return Text(input,
+        style: const TextStyle(
+            fontSize: 16,
+            color: Colors.white,
+            shadows: <Shadow>[
+              Shadow(
+                  offset: Offset(2.0, 2.0),
+                  blurRadius: 3.0,
+                  color: Color.fromARGB(255, 0, 0, 0))
+            ],
+            letterSpacing: 0.2));
+  }
 }
